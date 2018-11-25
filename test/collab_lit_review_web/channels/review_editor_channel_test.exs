05d8ce7960a2370c85715e8845066b97746c1cda
Nicholas <nick.patella@live.com>
2018-11-25 08:54:14 -0500

    added autogenerated channel

---
+++ test/collab_lit_review_web/channels/review_editor_channel_test.exs
@@ -0,0 +1,26 @@
+defmodule CollabLitReviewWeb.ReviewEditorChannelTest do
+  use CollabLitReviewWeb.ChannelCase
+
+  setup do
+    {:ok, _, socket} =
+      socket(CollabLitReviewWeb.UserSocket, "user_id", %{some: :assign})
+      |> subscribe_and_join(CollabLitReviewWeb.ReviewEditorChannel, "review_editor:lobby")
+
+    {:ok, socket: socket}
+  end
+
+  test "ping replies with status ok", %{socket: socket} do
+    ref = push socket, "ping", %{"hello" => "there"}
+    assert_reply ref, :ok, %{"hello" => "there"}
+  end
+
+  test "shout broadcasts to review_editor:lobby", %{socket: socket} do
+    push socket, "shout", %{"hello" => "all"}
+    assert_broadcast "shout", %{"hello" => "all"}
+  end
+
+  test "broadcasts are pushed to the client", %{socket: socket} do
+    broadcast_from! socket, "broadcast", %{"some" => "data"}
+    assert_push "broadcast", %{"some" => "data"}
+  end
+end
