5baac925d7ed0cfbdac22188e11ddb0eecf0d31a
Nicholas <nick.patella@live.com>
2018-11-26 18:28:09 -0500

    added state function for channel

--- lib/collab_lit_review_web/channels/review_editor_channel.ex
+++ lib/collab_lit_review_web/channels/review_editor_channel.ex
@@ -21,16 +21,16 @@ defmodule CollabLitReviewWeb.ReviewEditorChannel do
 
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
+  #def handle_in("ping", payload, socket) do
+  #  {:reply, {:ok, payload}, socket}
+  #end
 
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (review_editor:lobby).
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
-    {:noreply, socket}
-  end
+  #def handle_in("shout", payload, socket) do
+  #  broadcast socket, "shout", payload
+  #  {:noreply, socket}
+  #end
 
   def handle_in("add_collaborator_to_review", %{"user_email" => user_email}, socket) do
     review = Reviews.get_review(socket.assigns.review_id)
@@ -68,6 +68,72 @@ defmodule CollabLitReviewWeb.ReviewEditorChannel do
         else
           :unauthorized
         end
+    end
+  end
+
+
+  #
+  # State
+  #
+  # title:
+  # collaborators:
+  # swimlanes:
+  # buckets:
+  # papers:
+  # authors:
+
+  defp flatten(object, key) do
+    Map.put(object, key, Enum.uniq(Enum.map(object[key], fn x -> x.id end)))
+  end
+
+  defp flatten2(object, key) do
+    Map.put(object, key, object[key].id)
+  end
+
+  defp uniquify(object_list) do
+    Enum.uniq_by(object_list, fn object -> object.id end);
+  end
+
+  defp generate_client_view(user_id, review_id) do
+    case Reviews.get_review(review_id) do
+      nil -> :error
+      review ->
+        # Start with the review.
+        review = review |> Repo.preload([:collaborators, :swimlanes]
+        # Get collaborators and swimlanes from the review.
+        collaborators = review.collaborators
+        swimlanes = review.swimlanes |> Repo.preload(:buckets)
+        # Get buckets from the swimlanes
+        buckets = List.foldl(swimlanes, [], fn (swimlane, acc) ->
+          swimlane.buckets ++ acc
+        end) |> uniquify |> Repo.preload([:papers])
+        # Get papers from the buckets
+        papers = List.foldl(buckets, [], fn (bucket, acc) ->
+          bucket.papers ++ acc
+        end) |> uniquify |> Repo.preload([:authors, :references, :citations])
+        # Get authors, references, and citations from the papers
+        {authors, references, citations} = List.foldl(papers, {[], [], []}, fn (paper, acc) ->
+          {paper.authors ++ (acc |> elem(0)), paper.references ++ (acc |> elem(1)), paper.citations ++ (acc |> elem(3))}
+        end)
+        authors = uniquify(authors)
+        references = uniquify(references)
+        citations = uniquify(citations)
+        # Now flatten all those member lists to ids to save space
+        review = review |> flatten(:collaborators) |> flatten(:swimlanes)
+        swimlanes = swimlanes |> flatten(:buckets)
+        buckets = buckets |> flatten(:papers)
+        papers = papers |> flatten(:authors) |> flatten(:references) |> flatten(:citations)
+        # Construct and return the state
+        %{
+          "title" => review.title,
+          "collaborators" => collaborators,
+          "swimlanes" => swimlanes,
+          "buckets" => buckets,
+          "papers" => papers,
+          "authors" => authors,
+          "references" => references,
+          "citations" => citations,
+        }
     end
   end
 end
