c9d3c55df963e4723b05b40235fd29d2a6327be1
Nicholas <nick.patella@live.com>
2018-11-27 00:48:25 -0500

    made buttons clickable

--- assets/js/revieweditor.jsx
+++ assets/js/revieweditor.jsx
@@ -8,21 +8,14 @@ import $ from "jquery";
 import { getReviewEditorChannel } from './socket'
 
 import api from "./api"
-/*
-function ReviewEditor(props) {
-  console.log("Rendering review editor for", reviewId);
-  let channel = socket.channel("review_editor:" + review_id.toString(), {});
-
-  return <p>This will be an editor.</p>;
-}*/
 
 class ReviewEditor extends React.Component {
   constructor(props) {
     super(props);
 
-    console.log("ReviewEditor constructed with the following props, ", props)
     this.state = {
       review_id: props.review_id,
+      selected_bucket: null,
       review: null,
     };
   }
@@ -32,20 +25,48 @@ class ReviewEditor extends React.Component {
   }
 
   componentDidMount() {
-    console.log("Review editor component mounted with review_id", this.state.review_id);
     let channel = socket.channel("review_editor:" + this.state.review_id.toString(), {});
     this.connectToChannel(channel);
   }
 
   render() {
     if (!this.state.review) return null;
+
     console.log("Rendering!", this.state);
+
+    let onBucketClick = (id) => {
+      console.log("Clicked ", id);
+      this.setState(state => {
+        if (state.selected_bucket != null) {
+          return _.assign(state, {selected_bucket: null});
+        }
+        else {
+          return _.assign(state, {selected_bucket: id});
+        }
+      })
+    }
+
     let swimlanes = _.map(this.state.review.swimlanes, (swimlane) => {
-      return <Swimlane swimlane={swimlane} buckets={this.state.review.buckets}/>;
+      return <Swimlane swimlane={swimlane} buckets={this.state.review.buckets} onBucketClick={onBucketClick}/>;
     });
-    return <div className="container-fluid">
-      {swimlanes}
-    </div>;
+
+    if (this.state.selected_bucket == null) {
+      return <div className="container-fluid">
+        {swimlanes}
+      </div>;
+    }
+    else {
+      return <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-8">
+            {swimlanes}
+          </div>
+          <div className="col-md-4">
+            <p>Names of papers will go here</p>
+          </div>
+        </div>
+      </div>;
+    }
   }
 
   connectToChannel(channel) {
@@ -71,7 +92,7 @@ class ReviewEditor extends React.Component {
 function Swimlane(props) {
   let buckets = _.filter(props.buckets, (bucket) => (bucket.swimlane_id == props.swimlane.id));
   buckets = _.sortBy(buckets, ["position"]);
-  buckets = _.map(buckets, (bucket) => <div key={bucket.id} className="col-md-2"><Bucket bucket={bucket}/></div>)
+  buckets = _.map(buckets, (bucket) => <div key={bucket.id} className="col-md-2"><Bucket bucket={bucket} onBucketClick={props.onBucketClick}/></div>)
   return <div key={props.swimlane.id} className="swimlane card">
     <h3 className="card-title">{props.swimlane.name}</h3>
     <div className="row">
@@ -81,7 +102,7 @@ function Swimlane(props) {
 }
 
 function Bucket(props) {
-  return <button className="bucket card">
+  return <button onClick={() => props.onBucketClick(props.bucket.id)} className="bucket card">
     <h4 className="card-title text-center">{props.bucket.name}</h4>
     <h3 className="text-center">{props.bucket.papers.length}</h3>
   </button>
